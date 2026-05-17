d8b19ff8805d0067378f34897bd7fb19cfe293be
code6150 <code6150@gmail.com>
2026-05-17 20:40:48 +0900

    refactor: Change Controller to RestController

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
     @GetMapping("/introduce")
     public String introduce(@RequestParam(required = false, defaultValue = "권민상")String name, Model model) {
         return String.format("반갑습니다 제 이름은 %s이라고 합니다.",name);
