178ac9db98df2ee258c65be9ee83c9f2fdd11945
code6150 <code6150@gmail.com>
2026-05-18 13:19:20 +0900

    test: article page testing

--- src/main/java/com/example/demo/ArticleController.java
+++ src/main/java/com/example/demo/ArticleController.java
@@ -1,4 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ArticleController {
+
+    @GetMapping("/article/{id}")
+    public String getArticle() {
+        return "안뇽";
+    }
 }
