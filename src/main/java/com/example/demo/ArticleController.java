4360b0ab2f4aadfad04b8470759b13b26cfa4954
code6150 <code6150@gmail.com>
2026-05-18 22:02:10 +0900

    feat: added GetAPI in ArticleService

--- src/main/java/com/example/demo/ArticleController.java
+++ src/main/java/com/example/demo/ArticleController.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 @RestController
 public class ArticleController {
 
+    HashMap<Integer, Article> article = new HashMap<Integer, Article>();
+
     @GetMapping("/article/{id}")
-    public String getArticle() {
-        return "안뇽";
+    public Article getArticle(@PathVariable int id) {
+        return article.get(id);
     }
+
+    @PutMapping("/article/{id}")
+    public String putArticle() {return "a";}
+
+
 }
