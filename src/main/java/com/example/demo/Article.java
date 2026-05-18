32257234d80c3e9c01af4349dae768a6a7519240
code6150 <code6150@gmail.com>
2026-05-18 15:30:07 +0900

    chore: setter는 필요 없을듯

--- src/main/java/com/example/demo/Article.java
+++ src/main/java/com/example/demo/Article.java
@@ -11,12 +11,4 @@ public class Article {
     public String getDescription() {
         return description;
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
