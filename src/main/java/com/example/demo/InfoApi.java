007a97af34fc53d8a9975698297ae321029c2452
code6150 <code6150@gmail.com>
2026-05-18 12:33:30 +0900

    added getter, setter

--- src/main/java/com/example/demo/InfoApi.java
+++ src/main/java/com/example/demo/InfoApi.java
@@ -1,4 +1,23 @@
 package com.example.demo;
 
 public class InfoApi {
+
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
