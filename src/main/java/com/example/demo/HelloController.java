1c82096621820b532b96ea292f233e8ff0152ba3
code6150 <code6150@gmail.com>
2026-05-18 12:31:31 +0900

    feat: Json개인정보 저장용  info클래스 생성

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import ch.qos.logback.core.model.Model;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,5 +14,10 @@ public class HelloController {
     @GetMapping("/introduce")
     public String introduce(@RequestParam(required = false, defaultValue = "권민상")String name, Model model) {
         return String.format("반갑습니다 제 이름은 %s이라고 합니다.",name);
+    }
+
+    @GetMapping("/json")
+    public  info() {
+
     }
 }
