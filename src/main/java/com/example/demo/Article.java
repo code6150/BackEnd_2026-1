75dd70097ad7107dddcd70144a88e4d0acb925b6
code6150 <code6150@gmail.com>
2026-05-18 15:28:50 +0900

    feat: Generate Article getter & setter

--- src/main/java/com/example/demo/Article.java
+++ src/main/java/com/example/demo/Article.java
@@ -1,4 +1,22 @@
 package com.example.demo;
 
 public class Article {
+    private int id;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
