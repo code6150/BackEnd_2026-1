efcd4c8754fb3ca2b5a8c64f0ae77218437a8355
code6150 <code6150@gmail.com>
2026-05-20 22:15:03 +0900

    feat: Member 기초적인 임플란트

--- src/main/java/com/example/demo/Member.java
+++ src/main/java/com/example/demo/Member.java
@@ -1,4 +1,12 @@
 package com.example.demo;
 
 public class Member {
+    private Long id;
+    private String nickName;
+    private String emailAddress;
+    private String passWord;
+
+    public Long getId() {
+        return id;
+    }
 }
