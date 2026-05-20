7b20b13a32bdff933d4a1ea3638a33ddeea1e07a
code6150 <code6150@gmail.com>
2026-05-21 00:35:54 +0900

    feat: 1단 필요한걸 넣어

--- src/main/java/com/example/demo/Article.java
+++ src/main/java/com/example/demo/Article.java
@@ -1,14 +1,22 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 public class Article {
     private static Long followingId = 1L;
 
     private final Long id;
+    private final String title;
     private final String description;
+    private final LocalDateTime LDT;
+    private LocalDateTime lastModifiedTime;
 
-    public Article(String description) {
+    public Article(String title, String description) {
         this.id = followingId++;
+        this.title = title;
         this.description = description;
+        this.LDT = LocalDateTime.now();
+        lastModifiedTime = null;
     }
 
 
@@ -16,7 +24,19 @@ public class Article {
         return id;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public LocalDateTime getLDT() {
+        return LDT;
+    }
+
+    public void setLastModifiedTime(LocalDateTime lastModifiedTime) {
+        this.lastModifiedTime = lastModifiedTime;
     }
 }
