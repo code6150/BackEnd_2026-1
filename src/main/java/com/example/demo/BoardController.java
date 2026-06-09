75bbd42649f457634fc258ef620d05cea03f78e6
code6150 <code6150@gmail.com>
2026-06-09 18:47:23 +0900

    feat: member, board API 구현완료

--- src/main/java/com/example/demo/BoardController.java
+++ src/main/java/com/example/demo/BoardController.java
@@ -2,7 +2,6 @@ package com.example.demo;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
