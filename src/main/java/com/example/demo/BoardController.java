5e630452fced248c8a1281466a88724187e9071f
code6150 <code6150@gmail.com>
2026-06-09 18:12:00 +0900

    feat: board API 시작

---
+++ src/main/java/com/example/demo/BoardController.java
@@ -0,0 +1,50 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@Controller
+public class BoardController {
+
+
+    @ResponseBody
+    @GetMapping("/boards")
+    public HashMap<Long, Article> getArticles() {
+        return BoardService.getBoard();
+    }
+
+    @ResponseBody
+    @GetMapping("/article/{id}")
+    public ResponseEntity<?> getArticle(@PathVariable Long id) {
+        if (!articleService.getArticle().containsKey(id)) {
+            return ResponseEntity.status(404).body("존재하지 않는 게시물입니다.");
+        }
+        return ResponseEntity.ok(articleService.getArticle().get(id));
+    }
+
+    @ResponseBody
+    @PostMapping("/article")
+    public String createArticle(@RequestParam String title, @RequestParam String description) {
+        articleService.creatArticle(title, description);
+        return "생성 완료";
+    }
+
+    @ResponseBody
+    @DeleteMapping("/article/{id}")
+    public String deleteArticle(@PathVariable Long id) {
+        articleService.deleteArticle(id);
+        return "삭제 완료";
+    }
+
+    @ResponseBody
+    @PutMapping("/article/{id}")
+    public String updateArticle(@PathVariable Long id, @RequestBody Article updatedArticle) {
+        articleService.updateArticle(id, updatedArticle);
+        return "수정 완료";
+    }
+
+}
