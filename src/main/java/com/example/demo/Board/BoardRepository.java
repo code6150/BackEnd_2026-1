4efc097ef7aacaf3d0a7afe0c4cc43816730483d
code6150 <code6150@gmail.com>
2026-06-22 21:44:58 +0900

    feat: JDBC -> BoardRepository에 save 기능 추가

--- src/main/java/com/example/demo/Board/BoardRepository.java
+++ src/main/java/com/example/demo/Board/BoardRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.Board;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -8,8 +9,22 @@ import java.util.HashMap;
 public class BoardRepository {
 
     private final HashMap<Long, Board> articleBoards = new HashMap<>();
-    public BoardRepository() {
+    public BoardRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
         this.articleBoards.put(1L, new Board("자유게시판"));
+    }
+
+    private final JdbcTemplate jdbcTemplate;
+
+
+    public void save(Board board) {
+
+        String sql = """
+                INSERT INTO board(name)
+                VALUES (?)
+                """;
+
+        jdbcTemplate.update(sql, board.getBoardName());
     }
 
     public HashMap<Long, Board> findAll() {
