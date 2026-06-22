ea50a0e098d8bd9e7112827c80d80d7db6baeaef
code6150 <code6150@gmail.com>
2026-06-22 21:43:41 +0900

    feat: JDBC -> MemberRepository에 save 기능 추가

--- src/main/java/com/example/demo/Member/MemberRepository.java
+++ src/main/java/com/example/demo/Member/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.Member;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import java.util.HashMap;
 
@@ -8,10 +9,28 @@ public class MemberRepository {
 
     private final HashMap<Long, Member> members = new HashMap<>();
 
-    public MemberRepository() {
+    public MemberRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
         this.members.put(1L, new Member("회원1", "member1@gmail.com", "1111"));
         this.members.put(2L, new Member("회원2", "member2@gmail.com", "2222"));
         this.members.put(3L, new Member("회원3", "member3@gmail.com", "3333"));
+    }
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public void save(Member member) {
+
+        String sql = """
+                INSERT INTO member(name, email, password)
+                VALUES (?, ?, ?)
+                """;
+
+        jdbcTemplate.update(
+                sql,
+                member.getNickName(),
+                member.getEmailAddress(),
+                member.getPassWord()
+        );
     }
 
     public HashMap<Long, Member> findAll() {
